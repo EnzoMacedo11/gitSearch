64561b22d3eabdd902faf4c07b128551746ce41b
EnzoMacedo11 <enzomacedo15@gmail.com>
2023-08-02 01:27:10 -0300

    feat: v1 Screen Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,171 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import styled from "styled-components/native";
 
 import { useState } from "react";
 import Header from "../../components/header";
+import axios from "axios";
+import { Alert } from "react-native";
 
-export default function Home() {
-  const [name, setName] = useState<string>("");
-  return (
-    <View style={styles.container}>
-      <Header/>
-      <Text style={styles.inputName}> Digite o Nome do Usuário</Text>
-      <TextInput onChangeText={setName} value={name} placeholder="..." />
-    </View>
-  );
-
-
+interface GitUser {
+  avatar_url: string;
+  name: string;
+  login: string;
+  location: string;
 
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1
-  },
+export default function Home() {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<GitUser>({ 
+    avatar_url: '',
+    name: '',
+    login: '',
+    location: '',
+  }); // Precisa ser coletado > E que no resultado apareça a foto, o nome, o login e a localização.
 
-  inputName:{
-    fontSize:18,
-    fontWeight:"600",
-    color:"#000000"
+  async function getUser() {
+    if(name == ""){
+      Alert.alert("Error","Digite um nome antes de buscar")
+    }else{
+      const url = `https://api.github.com/users/${name}`;
+    
+      try {
+        const response = await axios.get(url);
+        setUser(response.data);
+        console.log(response.data);
+      } catch (error) {
+        Alert.alert("Error",`O usuário "${name}" não foi encontrado.`)
+        console.log('Erro ao buscar usuário:', error);
+        setUser({ 
+          avatar_url: '',
+          name: '',
+          login: '',
+          location: '',
+        });
+      }
+    }
+   
   }
+  
 
-})
+  return (
+    <Main>
+      <Header />
+      <FormBox>
+        <InputName> Digite o Nome do Usuário</InputName>
+
+        <TextInputStyled
+          onChangeText={setName}
+          value={name}
+          placeholder="..."
+        />
+        <FormButton onPress={() => getUser()}>
+          <ButtonText>Buscar</ButtonText>
+        </FormButton>
+        <ResultBox>
+          <ImageBox>
+            <UserImage source={{ uri: user.avatar_url }} />
+          </ImageBox>
+
+          <InfoBox>
+            <UserName>{user.name}</UserName>
+            <UserLogin>{user.login}</UserLogin>
+            <UserLocation>{user.location}</UserLocation>
+          </InfoBox>
+        </ResultBox>
+        <ListButton onPress={() => getUser()}>
+          <ButtonText>Histórico de busca</ButtonText>
+        </ListButton>
+      </FormBox>
+    </Main>
+  );
+}
+
+const Main = styled.View`
+  display: flex;
+  
+`;
+const FormBox = styled.View`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  justify-content:center;
+ 
+`;
+
+const InputName = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+  color: #000000;
+`;
+
+const TextInputStyled = styled.TextInput`
+  margin-top: 15px;
+  height: 35px;
+  width: 80%;
+  border-radius: 5px;
+  background-color: grey;
+  text-align: center;
+`;
+
+const FormButton = styled.TouchableOpacity`
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #001f3f;
+  width: 30%;
+  height: 35px;
+  border-radius: 15px;
+`;
+
+const ListButton = styled.TouchableOpacity`
+  margin-top: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #001f3f;
+  width: 45%;
+  height: 35px;
+  border-radius: 15px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+`;
+
+const ResultBox = styled.View`
+  margin-top: 25px;
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  height: 300px;
+  
+  border-radius:15px;
+`;
+const InfoBox = styled.View`
+  display: flex;
+  justify-content:center;
+  margin-left:25px;
+`;
+const ImageBox = styled.View`
+  margin-left: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UserName = styled.Text`
+  font-size: 18;
+`;
+
+const UserLogin = styled.Text`
+  font-size: 18;
+`;
+const UserLocation = styled.Text`
+  font-size: 18;
+`;
+const UserImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+`;
